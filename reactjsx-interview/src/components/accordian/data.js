7558d45de04516cc5f1470228a20dc24165f2e7c
Slonetech <kengeregordon@gmail.com>
2024-02-26 23:42:13 +0300

    dummy data

---
+++ reactjsx-interview/src/components/accordian/data.js
@@ -0,0 +1,23 @@
+const data = [
+  {
+    id : 1,
+    title : 'Section 1',
+    content : 'Content 1'
+  },
+  {
+    id : 2,
+    title : 'Section 2',
+    content : 'Content 2'
+  },
+  {
+    id : 3,
+    title : 'Section 3',
+    content : 'Content 3'
+  },
+  {
+    id : 4,
+    title : 'Section 4',
+    content : 'Content 4'
+  }
+
+]
